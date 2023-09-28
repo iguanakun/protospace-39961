ec6ddcbd376003576b7b78065300fb175ddee979
iguanakun <yw16027@gmail.com>
2023-09-28 15:36:43 +0900

    レビュー指摘修正
    以下を修正
    ・ログイン状態のユーザーであっても、他のユーザーのプロトタイプ編集ページのURLを直接入力して遷移しようとすると、トップページにリダイレクトされること

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,7 +27,7 @@ class PrototypesController < ApplicationController
   def edit
     prototype = Prototype.find(params[:id])
     unless current_user.id == prototype.user_id
-      redirect_to root_pathx
+      redirect_to root_path
     end
     @prototype = Prototype.find(params[:id])
   end
